49f0659ff64bbd6235b3cfb8401ea730b5e6a349
Arslan Ahmad <ars.ahm6@gmail.com>
2021-04-24 18:20:51 +0500

    Finally could run react app

---
+++ Revisited/my-app/src/components/first.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class FirstComponent extends Component {
+    render() {
+        return (<h1>Hello Component</h1>)
+    }
+}
+
+export default FirstComponent
